17c275e8dfd71b949db72ca1b99ee8054721a373
cari-fariaz <fariazcarina@gmail.com>
2022-06-29 17:15:24 -0300

    suma y resta.py

--- Tp1.py
+++ Tp1.py
@@ -6,3 +6,11 @@ num1=int(input("Ingrese el primer numero a calcular:  "))
 num2=int(input("Ingrese el segundo numero a calcular:  "))
 num3=int(input("Ingrese el tercer numero a calcular:  "))
 sumando (num1,num2,num3)
+
+def restando(num4,num5): 
+    resultado = num4-num5
+    print(f"El resultado de la operación resta es:{resultado}")
+
+num4=int(input("Ingrese el quinto numero a calcular:  "))
+num5=int(input("Ingrese el sexto numero a calcular:  "))
+restando (num4,num5)
