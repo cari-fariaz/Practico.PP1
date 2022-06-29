f27370f46988ebd4a2de818e9e895c99b996eb23
cari-fariaz <fariazcarina@gmail.com>
2022-06-29 16:50:54 -0300

    ultimo

---
+++ Tp1.py
@@ -0,0 +1,8 @@
+def sumando ( num1, num2, num3):
+    resultado = num1+num2+num3
+    print(f"El resultado de la operación suma es:{resultado}")
+
+num1=int(input("Ingrese el primer numero a calcular:  "))
+num2=int(input("Ingrese el segundo numero a calcular:  "))
+num3=int(input("Ingrese el tercer numero a calcular:  "))
+sumando (num1,num2,num3)
